8dce48f23644b9f85eecefc9fad5403b865cf0e3
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-12-13 11:15:00 +0530

    env and backup

---
+++ React-Frontend/constants/constants.js
@@ -0,0 +1,10 @@
+const config = {
+  PORT: import.meta.env.VITE_PORT || 3001,
+  GQL_BACKEND_URL: import.meta.env.VITE_GQL_BACKEND_URL || "http://localhost:4",
+  REST_BACKEND_GET_URL: import.meta.env.VITE_REST_BACKEND_GET_URL || "http://localhost:3000/",
+  REST_BACKEND_PUT_URL: import.meta.env.VITE_REST_BACKEND_PUT_URL || "http://localhost:3000/",
+  REST_BACKEND_POST_URL: import.meta.env.VITE_REST_BACKEND_POST_URL || "http://localhost:3000/",
+  REST_BACKEND_DELETE_URL: import.meta.env.VITE_REST_BACKEND_DELETE_URL || "http://localhost:3000/",
+};
+
+export default config;
