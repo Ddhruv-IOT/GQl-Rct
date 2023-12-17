31f68cffc0280f4e345533906a0d0e788af8b667
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-12-17 16:15:00 +0530

    fixed values or static

--- React-Frontend/constants/constants.js
+++ React-Frontend/constants/constants.js
@@ -1,10 +1,19 @@
 const config = {
   PORT: import.meta.env.VITE_PORT || 3001,
-  GQL_BACKEND_URL: import.meta.env.VITE_GQL_BACKEND_URL || "http://localhost:4",
-  REST_BACKEND_GET_URL: import.meta.env.VITE_REST_BACKEND_GET_URL || "http://localhost:3000/",
-  REST_BACKEND_PUT_URL: import.meta.env.VITE_REST_BACKEND_PUT_URL || "http://localhost:3000/",
-  REST_BACKEND_POST_URL: import.meta.env.VITE_REST_BACKEND_POST_URL || "http://localhost:3000/",
-  REST_BACKEND_DELETE_URL: import.meta.env.VITE_REST_BACKEND_DELETE_URL || "http://localhost:3000/",
+  GQL_BACKEND_URL:
+    import.meta.env.VITE_GQL_BACKEND_URL || "http://localhost:4000",
+  REST_BACKEND_GET_URL:
+    import.meta.env.VITE_REST_BACKEND_GET_URL ||
+    "http://localhost:3001/api/todos",
+  REST_BACKEND_PUT_URL:
+    import.meta.env.VITE_REST_BACKEND_PUT_URL ||
+    "http://localhost:3001/api/todos",
+  REST_BACKEND_POST_URL:
+    import.meta.env.VITE_REST_BACKEND_POST_URL ||
+    "http://localhost:3001/api/todos",
+  REST_BACKEND_DELETE_URL:
+    import.meta.env.VITE_REST_BACKEND_DELETE_URL ||
+    "http://localhost:3001/api/todos",
 };
 
 export default config;
