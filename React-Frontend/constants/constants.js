f791fcc3819ded729abf7a42263941683075ecdd
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-12-31 13:55:00 +0530

    test

--- React-Frontend/constants/constants.js
+++ React-Frontend/constants/constants.js
@@ -4,16 +4,16 @@ const config = {
     import.meta.env.VITE_GQL_BACKEND_URL || "https://gql-api-mcxt.onrender.com/",
   REST_BACKEND_GET_URL:
     import.meta.env.VITE_REST_BACKEND_GET_URL ||
-    "https://rest-api-dihw.onrender.com/api/todos",
+    "https://rest-api-d1hw.onrender.com/api/todos",
   REST_BACKEND_PUT_URL:
     import.meta.env.VITE_REST_BACKEND_PUT_URL ||
-    "https://rest-api-dihw.onrender.com/api/todos",
+    "https://rest-api-d1hw.onrender.com/api/todos",
   REST_BACKEND_POST_URL:
     import.meta.env.VITE_REST_BACKEND_POST_URL ||
-    "https://rest-api-dihw.onrender.com/api/todos",
+    "https://rest-api-d1hw.onrender.com/api/todos",
   REST_BACKEND_DELETE_URL:
     import.meta.env.VITE_REST_BACKEND_DELETE_URL ||
-    "https://rest-api-dihw.onrender.com/api/todos",
+    "https://rest-api-d1hw.onrender.com/api/todos",
 };
 
 export default config;
