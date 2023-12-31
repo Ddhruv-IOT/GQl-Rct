1157935ba51062502cb732dd4b5b58c39e92e016
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-12-31 13:55:00 +0530

    allow all

--- REST-API/nodeBackend/index.js
+++ REST-API/nodeBackend/index.js
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://todo-app-04bu.onrender.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
