06d531739f15e84d0bd34ca363261e1c9f1bb9d1
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-12-31 13:55:00 +0530

    allow all

--- REST-API/nodeBackend/index.js
+++ REST-API/nodeBackend/index.js
@@ -57,15 +57,30 @@ app.use((req, res, next) => {
   res.setHeader('Referrer-Policy', 'same-origin');
   next();
 });
-
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+const allowedOrigins = ['https://todo-app-04bu.onrender.com', 'https://rest-gql-todo-app.netlify.app/'];
+
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Referrer-Policy', 'same-origin');
   next();
 });
+
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://todo-app-04bu.onrender.com');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   res.setHeader('Referrer-Policy', 'same-origin');
+//   next();
+// });
 
 app.use(bodyParser.json());
 
