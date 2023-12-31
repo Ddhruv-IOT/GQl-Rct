d0eb163f3c73697cf0197bfae46cc08a9cebcd5b
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-12-31 10:55:00 +0530

    change

--- REST-API/nodeBackend/constants/constants.js
+++ REST-API/nodeBackend/constants/constants.js
@@ -3,5 +3,5 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT || 3001,
-  FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:5173",
+  FRONTEND_URL: process.env.FRONTEND_URL || "https://rest-gql-todo-app.netlify.app/",
 };
