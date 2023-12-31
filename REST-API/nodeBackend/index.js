eac45d288a990f5622af9cee48fb8400ed0b214d
Ddhruv-IOT <ddhruvarora2@gmail.com>
2024-01-01 00:55:00 +0530

    cleaned the code

--- REST-API/nodeBackend/index.js
+++ REST-API/nodeBackend/index.js
@@ -1,64 +1,21 @@
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const todosRouter = require("./routers/routers");
-
-// const { PORT, FRONTEND_URL } = require("./constants/constants");
-
-// const app = express();
-
-// // app.use((req, res, next) => {
-// //   res.header("Access-Control-Allow-Origin", FRONTEND_URL);
-// //   // You can also use '*' if you want to allow any origin
-// //   // res.header("Access-Control-Allow-Origin", "*");
-// //   next();
-// // });
-
-// // Enable other CORS-related headers and methods
-// app.use(cors(
-//   {
-//     origin:"https://todo-app-04bu.onrender.com/"
-//   }
-// ));
-
-// app.use((req, res, next) => {
-//   res.setHeader('Referrer-Policy', 'same-origin');
-//   next();
-// });
-// app.use(bodyParser.json());
-
-// app.use(todosRouter);
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const todosRouter = require("./routers/routers");
 
-const { PORT, FRONTEND_URL } = require("./constants/constants");
+const { PORT, FRONTEND_URL1, FRONTEND_URL2 } = require("./constants/constants");
 
 const app = express();
 
-// Enable other CORS-related headers and methods
-
-// app.use(cors({
-//   origin: "https://todo-app-04bu.onrender.com"
-// }));
-
 app.options('*', cors())
-
-// app.options('*', cors());
 
 app.use((req, res, next) => {
   res.setHeader('Referrer-Policy', 'same-origin');
   next();
 });
+
 app.use((req, res, next) => {
-const allowedOrigins = ['https://todo-app-04bu.onrender.com', 'https://rest-gql-todo-app.netlify.app'];
+  const allowedOrigins = [FRONTEND_URL1, FRONTEND_URL2];
 
   const origin = req.headers.origin;
 
@@ -72,15 +29,6 @@ const allowedOrigins = ['https://todo-app-04bu.onrender.com', 'https://rest-gql-
   res.setHeader('Referrer-Policy', 'same-origin');
   next();
 });
-
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'https://todo-app-04bu.onrender.com');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//   res.setHeader('Access-Control-Allow-Credentials', 'true');
-//   res.setHeader('Referrer-Policy', 'same-origin');
-//   next();
-// });
 
 app.use(bodyParser.json());
 
