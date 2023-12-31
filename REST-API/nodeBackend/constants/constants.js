436d4fe520364975ebb7f157ee5050d24f7ff1a7
Ddhruv-IOT <ddhruvarora2@gmail.com>
2024-01-01 00:55:00 +0530

    cleaned the code

--- REST-API/nodeBackend/constants/constants.js
+++ REST-API/nodeBackend/constants/constants.js
@@ -3,5 +3,7 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT || 3001,
-  FRONTEND_URL: process.env.FRONTEND_URL || "https://rest-gql-todo-app.netlify.app/",
+  FRONTEND_URL1: process.env.FRONTEND_URL1 || 'https://todo-app-04bu.onrender.com',
+  FRONTEND_URL2: process.env.FRONTEND_URL2 || 'https://rest-gql-todo-app.netlify.app', 
+
 };
