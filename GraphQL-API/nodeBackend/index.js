c949ecb2d9bfd403b436955b565f6e66c5795399
Ddhruv-IOT <ddhruvarora2@gmail.com>
2024-01-01 00:55:00 +0530

    removed unused lines

--- GraphQL-API/nodeBackend/index.js
+++ GraphQL-API/nodeBackend/index.js
@@ -46,7 +46,6 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  // cors: { origin: FRONTEND_URL },
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
